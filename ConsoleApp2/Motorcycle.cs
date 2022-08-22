8a64251028c60e911f078550402a9cb3db6555d2
Andrei Spurhiash <spy.dru@gmail.com>
2022-08-22 20:44:41 +0200

    add task

---
+++ ConsoleApp2/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Motorcycle
+    {
+        public Guid Id { get; private set; }
+        public string Model { get; set; }
+        public int Odometer { get; set; }
+        public string MadeIn { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id - {Id}, model - {Model}, Odometer - {Odometer}, Made - {MadeIn}";
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp2;
+
+
+Motorcycle[] arr = new Motorcycle[5];
+
+arr[0] = new Motorcycle { Model = "Minsk X250", MadeIn = "China", Odometer = 6_000 };
+arr[1] = new Motorcycle { Model = "Honda CB500F", MadeIn = "Japan", Odometer = 11_500 };
+arr[2] = new Motorcycle { Model = "BMW G1200R", MadeIn = "Germany", Odometer = 20_000 };
+arr[3] = new Motorcycle { Model = "Triumph Tiger 800", MadeIn = "China", Odometer = 30_000 };
+arr[4] = new Motorcycle { Model = "Honda CB1100RR", MadeIn = "Japan", Odometer = 26_000 };
+
+
+void FirstOrDefault(Motorcycle[] arr2, Func<Motorcycle, bool> result)
+{
+    bool results = false;
+
+    foreach (var moto in arr2)
+    {
+        if (result(moto))
+        {
+            results = true;
+            Console.WriteLine(moto.ToString());
+            results = true;
+        }
+    }
+    if (results == false) Console.WriteLine(arr[0].ToString());
+}
+
+
+FirstOrDefault(arr, moto => moto.Odometer >= 50_000);
+
+FirstOrDefault(arr, moto => moto.MadeIn.Equals("China"));
+
+FirstOrDefault(arr, moto => moto.Odometer >= 25_000 && moto.MadeIn.Equals("Japan"));
