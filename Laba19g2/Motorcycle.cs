c744152bfcb1493c2cda5dce8a259f4f0e75a227
Grizzly <medved1991alexander@gmail.com>
2022-08-22 22:32:09 +0300

    performed

---
+++ Laba19g2/Motorcycle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba19g2
+{
+    public class Motorcycle
+    {
+        public Guid Id { get; private set; }
+        public string Model { get; set; }
+        public int Odometer { get; set; }
+        public string MadeIn { get; set; }
+
+        
+        public Motorcycle FirstOrDefault(Motorcycle[] motorcycles, Func<Motorcycle, bool> predicate)
+        {
+            foreach (var moto in motorcycles)
+            {
+                if (predicate(moto))
+                {
+                    return moto;
+                }
+            }
+            return motorcycles[default];
+        }        
+    }
+}
---
+++ Laba19g2/Program.cs
@@ -0,0 +1,19 @@
+﻿using Laba19g2;
+
+Motorcycle[] arr = new Motorcycle[5];
+
+arr[0] = new Motorcycle { Model = "Minsk X250", MadeIn = "China", Odometer = 6_000 };
+arr[1] = new Motorcycle { Model = "Honda CB500F", MadeIn = "Japan", Odometer = 11_500 };
+arr[2] = new Motorcycle { Model = "BMW G1200R", MadeIn = "Germany", Odometer = 20_000 };
+arr[3] = new Motorcycle { Model = "Triumph Tiger 800", MadeIn = "China", Odometer = 30_000 };
+arr[4] = new Motorcycle { Model = "Honda CB1100RR", MadeIn = "Japan", Odometer = 26_000 };
+
+Motorcycle myMoto = new();
+
+var moto1 = myMoto.FirstOrDefault(arr, m => m.Odometer >= 50_000);
+var moto2 = myMoto.FirstOrDefault(arr, m => m.MadeIn.Equals("Chine",StringComparison.OrdinalIgnoreCase));
+var moto3 = myMoto.FirstOrDefault(arr, m => m.Odometer >= 25_000 && m.MadeIn.Equals("Japan", StringComparison.OrdinalIgnoreCase));
+
+Console.WriteLine(moto1.Model);
+Console.WriteLine(moto2.Model);
+Console.WriteLine(moto3.Model);
